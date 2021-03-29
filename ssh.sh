76f75708a8b8f539d5a825ab7942d3eac0af937d
Taras Glek <taras@glek.net>
2021-03-28 18:45:25 -0700

    works on phone

--- ssh.sh
+++ ssh.sh
@@ -1,1 +1,2 @@
-ssh -e none -t -q -x prom socat -v - udp4-sendto:wireguard:5000,sourceport=9000
+# ssh -e none -t -q -x prom socat -v - udp4-sendto:wireguard:5000,sourceport=9000
+ssh -e none -t -q -x localhost -p 8022 socat -v - udp4-sendto:wireguard:5000,sourceport=9000
